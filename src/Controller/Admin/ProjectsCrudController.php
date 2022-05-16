aa5e4804cf45902a557cdf104446cb4dcde152c7
mgrab355 <matgrab355@gmail.com>
2022-05-16 22:01:22 +0200

    easyadmin_dodatkowe_kolumny

--- src/Controller/Admin/ProjectsCrudController.php
+++ src/Controller/Admin/ProjectsCrudController.php
@@ -4,6 +4,10 @@ namespace App\Controller\Admin;
 
 use App\Entity\Projects;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 
 class ProjectsCrudController extends AbstractCrudController
 {
@@ -12,14 +16,15 @@ class ProjectsCrudController extends AbstractCrudController
         return Projects::class;
     }
 
-    /*
+
     public function configureFields(string $pageName): iterable
     {
         return [
             IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            TextField::new('name'),
+            TextEditorField::new('url' ),
+            DateField::new('date'),
         ];
     }
-    */
+
 }
